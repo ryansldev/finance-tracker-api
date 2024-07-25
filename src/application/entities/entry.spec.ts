33deac8ca188e3dfa95e806e4c77b2d5977bcfbe
ryansldev <ryanlima1309@gmail.com>
2024-07-25 15:33:37 -0300

    feat: add entry entity

---
+++ src/application/entities/entry.spec.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "vitest";
+import { User } from './user'
+import { Dashboard } from './dashboard'
+import { EntryCategory } from "./entry-category";
+import { Entry } from './entry'
+
+describe('Entry', () => {
+  it('should be able to create a entry', () => {
+    const user = new User({
+      name: 'John',
+      lastname: 'Doe',
+      email: 'johndoe@gmail.com',
+      password: 'johndoe123'
+    })
+
+    const dashboard = new Dashboard({
+      title: 'Dashboard',
+      authorId: user.id,
+    })
+
+    const entryCategory = new EntryCategory({
+      title: 'Transport',
+      color: '#000',
+    })
+
+    const entry = new Entry({
+      title: 'Passport',
+      value: 300,
+      dashboardId: dashboard.id,
+      categoryId: entryCategory.id,
+    })
+
+    expect(entry).toBeInstanceOf(Entry)
+  })
+})
---
+++ src/application/entities/entry.ts
@@ -0,0 +1,73 @@
+import { randomUUID } from "crypto";
+import { Replace } from "../../helpers/Replace";
+
+interface EntryProps {
+  id: string;
+  title: string;
+  description?: string;
+  value: number;
+  date: Date;
+  categoryId: string;
+  dashboardId: string;
+}
+
+type EntryPropsBody = Replace<EntryProps, {
+  id?: string;
+  date?: Date;
+}>
+
+export class Entry {
+  private props: EntryProps
+
+  constructor(props: EntryPropsBody) {
+    this.props = {
+      id: props.id ?? randomUUID(),
+      ...props,
+      date: new Date(),
+    }
+  }
+
+  get id(): string {
+    return this.props.id
+  }
+
+  get categoryId(): string {
+    return this.props.categoryId
+  }
+
+  get dashboardId(): string {
+    return this.props.dashboardId
+  }
+
+  get title(): string {
+    return this.props.title
+  }
+
+  set title(title: string) {
+    this.props.title = title
+  }
+
+  get description(): string | undefined {
+    return this.props.description
+  }
+
+  set description(description: string) {
+    this.props.description = description
+  }
+
+  get value(): number {
+    return this.props.value
+  }
+
+  set value(value: number) {
+    this.props.value = value
+  }
+
+  get date(): Date {
+    return this.props.date
+  }
+
+  set date(date: Date) {
+    this.props.date = date
+  }
+}
