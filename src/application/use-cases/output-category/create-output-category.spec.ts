3a8a210e173ce51090c69701ff660ce8dfa6ce3a
ryansldev <ryanlima1309@gmail.com>
2024-08-07 14:44:47 -0300

    feat: add create output use-case

---
+++ src/application/use-cases/output-category/create-output-category.spec.ts
@@ -0,0 +1,41 @@
+import { describe, expect, test } from 'vitest'
+
+import { OutputCategory } from '../../entities/output-category'
+
+import { InMemoryUsersRepository } from '../../repositories/in-memory/in-memory-users-repository'
+import { InMemoryDashboardsRepository } from '../../repositories/in-memory/in-memory-dashboards-repository'
+import { InMemoryOutputCategoriesRepository } from '../../repositories/in-memory/in-memory-output-categories'
+
+import { CreateUser } from '../user/create-user'
+import { CreateDashboard } from '../dashboard/create-dashboard'
+import { CreateOutputCategory } from './create-output-category'
+
+describe('Create Output Category', () => {
+  test('should be able to create a output category', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const dashboardsRepository = new InMemoryDashboardsRepository()
+    const outputCategoriesRepository = new InMemoryOutputCategoriesRepository()
+
+    const createUser = new CreateUser(usersRepository)
+    const createDashboard = new CreateDashboard(dashboardsRepository)
+    const sut = new CreateOutputCategory(dashboardsRepository, outputCategoriesRepository)
+
+    const user = await createUser.execute({
+      name: 'John',
+      lastname: 'Doe',
+      email: 'johndoe@gmail.com',
+      password: 'johndoe123'
+    })
+
+    const dashboard = await createDashboard.execute({
+      title: 'Personal Finances',
+      authorId: user.id,
+    })
+
+    expect(sut.execute({
+      title: 'Transport',
+      color: '#000',
+      dashboardId: dashboard.id,
+    })).resolves.toBeInstanceOf(OutputCategory)
+  })
+})
---
+++ src/application/use-cases/output-category/create-output-category.ts
@@ -0,0 +1,37 @@
+import { OutputCategory } from '../../entities/output-category';
+import { DashboardsRepository } from '../../repositories/dashboards-repository'
+import { OutputCategoriesRepository } from '../../repositories/output-categories-repository'
+
+import { DashboardNotFound } from '../dashboard/errors/DashboardNotFound'
+
+interface CreateOutputCategoryRequest {
+  title: string;
+  color: string;
+  dashboardId: string;
+}
+
+export class CreateOutputCategory {
+  constructor (
+    private dashboardsRepository: DashboardsRepository,
+    private outputCategoriesRepository: OutputCategoriesRepository,
+  ) {}
+
+  async execute({
+    title,
+    color,
+    dashboardId
+  }: CreateOutputCategoryRequest) {
+    const dashboard = await this.dashboardsRepository.find(dashboardId)
+    if(!dashboard) throw new DashboardNotFound();
+
+    const outputCategory = new OutputCategory({
+      title,
+      color,
+      dashboardId,
+    })
+
+    await this.outputCategoriesRepository.create(outputCategory)
+
+    return outputCategory
+  }
+}
